78056a7d03def4d5b4b4b13b6b220c3a8c6e8c20
DESKTOP-2938Q46\dpqls <dpqls03@gmail.com>
2021-01-12 17:41:13 +0900

    hello .java

---
+++ sample_github/src/com/kbigdata/edu/HellowWorld.java
@@ -0,0 +1,40 @@
+package com.kbigdata.edu;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HellowWorld
+ */
+@WebServlet("/hw")
+public class HellowWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HellowWorld() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	System.out.println("HelloWorld~~~~~~");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+}
